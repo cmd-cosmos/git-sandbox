bc08147c2e5fd31eb10ce3f8a8013562f679f78c
Premanshu Bhagat <premtsapj@gmail.com>
2025-10-28 21:40:35 -0400

    working on isolating modified and staged(A) files to improve the auto git tracker

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import subprocess
 def getStatus():
   proc = os.system("git status")
   print("\nos.system exec: ", proc)
-  proc2 = subprocess.run(["git", "status"], stdout = True, text = True, check = False)
+  proc2 = subprocess.run(["git", "diff", "--cached", "--exit-code"], text = True, check = False)
   print("sub process output: \n", proc2)
 
 getStatus()
+print("-"*70)
+subprocess.run(["git", "diff", "--name-only"],stdout = True, text = True, check = False)
