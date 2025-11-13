1497eea07d4385bc7332f5173943210bc6ed63dc
Premanshu Bhagat <premtsapj@gmail.com>
2025-11-12 21:07:53 -0500

    created a complete list of modified and untracked files.

--- m2.py
+++ m2.py
@@ -11,15 +11,22 @@ def check_untracked():
     proc = subprocess.run(["git", "ls-files", "-o"], stdout=subprocess.PIPE, text=True)
 
     lst = list()
-    for i,j in enumerate(proc.stdout.splitlines(), start=1):
+    start_val = 1
+    for i,j in enumerate(proc.stdout.splitlines(), start=start_val):
         print(i, j)
         lst.append(j)
     
     proc2 = subprocess.run(["git", "diff", "--name-only"], text=True, stdout=subprocess.PIPE)
 
+    for i,j in enumerate(proc2.stdout.splitlines(), start=(start_val+1)):
+        print(i, j)
+        lst.append(j)
+    
+    print(lst)
+
+check_untracked()
 
 def diff_check():
     print("diff check: ")
     a = subprocess.run(["git", "diff", "--name-only"], text=True, check=False)
 
-check_untracked()
